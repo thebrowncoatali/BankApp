693775a91a7a49b739aa1d26fd98d7c0cec534e9
Ali Hamza <thebrowncoatali@gmail.com>
2020-11-25 17:06:52 -0400

    Account types classes added [SavingsAccount / ChecquingAccount]

--- models/account.py
+++ models/account.py
@@ -46,4 +46,63 @@ class Account():
 		return self._current_balance
 
 
+class SavingsAccount(Account):
+	"""
+		Inherit form Account class.
+		this class is a specific account type:
+
+		Saving Account:
+
+		additional parameter ==> minimum_balance
+	"""
+
+	def __init__(self, **kwargs):
+		self._minimum_balance = kwargs.pop('minimum_balance')
+		super().__init__(**kwargs)
+
+	def get_minimum_balance(self):
+		"""
+			return minimum balance
+		"""
+		return self._minimum_balance
+
+	def withdraw(self, amount):
+		"""
+			These accounts require account holders to maintain 
+			a minimum balance in the account. 
+			E.g. if the minimum balance is 5000 CAD and
+			the current balance in the account is 7000 CAD, 
+			the maximum withdrawal that can be allowed is 2000 CAD
+		"""
+		pass
+
+
+class ChecquingAccount(Account):
+	"""
+		Inherit form Account class.
+		this class is a specific account type:
+
+		Checquing Account:
+
+		additional parameter ==> overdraft_allowed
+	"""
+
+	def __init__(self, **kwargs):
+		self._overdraft_allowed = kwargs.pop('overdraft_allowed')
+		super().__init__(**kwargs)
+
+	def get_overdraft_allowed(self):
+		"""
+			return overdraft allowed
+		"""
+		return self._overdraft_allowed
+
+	def withdraw(self, amount):
+		"""
+			These accounts allow overdrafts. 
+			This means if an account has an overdraft limit of 5000 CAD, 
+			the account holder is allowed to withdraw up to 5000 CAD 
+			more than the money they have in the account.
+		"""
+		pass
 
