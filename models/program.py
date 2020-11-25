4694505214ef9cc558e2de0eb822a022e55d7bee
Ali Hamza <thebrowncoatali@gmail.com>
2020-11-25 17:24:13 -0400

    ask for a new account information

--- models/program.py
+++ models/program.py
@@ -51,6 +51,42 @@ class Program():
 			if not choice:
 				print('\nInvalid choice, please try again. choice a number between [1-3]')
 
+		if choice == 1:
+			# Create a new account
+			# ask details to the user
+			new_account = {}
+			new_account['account_holder_name'] = input('Enter account holder name: ')
+
+			print(
+				'Account types:',
+				' ** Saving Account [1]',
+				' ** Checquing Account [2]',
+				sep='\n'
+			)
+
+			choice = None
+			while not choice:
+				# Ask user choice
+				choice = input('Please choice an option: ')
+
+				# Validate choice
+
+				# First validation:
+				try:
+					choice = int(choice)
+				except:
+					return None
+
+				# Second Validation
+				if choice > 2 or choice < 1:
+					choice = None
+
+				# Error message
+				if not choice:
+					print('\nInvalid choice, please try again. choice a number between [1-2]')
+
+
+
 		if choice == 2:
 			# show second menu by the momment
 			self.show_account_menu()
